23141adce75da93cc3ea55d562465234a36d9ce6
juangr18 <juanr970318@gmail.com>
2022-02-15 09:51:08 -0500

    Changes on file pet.js

--- backend/models/pet.js
+++ backend/models/pet.js
@@ -2,6 +2,10 @@ import mongo from "mongoose";
 
 const schema = new mongo.Schema({
     name:string,
-    ,
+    owner:{type:mongo.Schema.ObjectId, ref:"owner"},
     race:{type:mongo.Schema.ObjectId,ref:"type_pet"},
 });
+
+const pet = mongo.model("pet", schema);
+
+export default pet;
