a52a995065a06ef3f59725bff783332b2f0c50f8
juangr18 <juanr970318@gmail.com>
2022-02-15 10:27:09 -0500

    Add connection to MongoDB in the file index.js

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-app.listen(process.env.PORT, () =>{
-    console.log("Server running on PORT", process.env.PORT);
+app.listen(process.env.PORT, () => {
+  console.log("Server running on PORT", process.env.PORT);
 });
 
-db
+db.dbConnection();
