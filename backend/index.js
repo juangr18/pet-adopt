61a6eea0ba8bead4febb938cf62ba29836f994f4
JoseHM <josehm1999@gmail.com>
2022-02-15 10:45:38 -0500

    Controller and Route for owner add

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,18 @@ import express from "express";
 import cors from "cors";
 import db from "./db/db.js";
 import dotenv from "dotenv";
+
+import ownerRoutes from "./routes/owner.route.js"
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/api/owner/", ownerRoutes);
 
-
-app.listen(process.env.PORT, () =>{
-    console.log("Server running on PORT", process.env.PORT);
+app.listen(process.env.PORT, () => {
+  db.dbConnection;
+  console.log("Server running on PORT", process.env.PORT);
 });
 
-db
+
