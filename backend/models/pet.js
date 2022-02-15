2a4b219588d10c49d89c7cf86cd777fefd45b499
juangr18 <juanr970318@gmail.com>
2022-02-15 10:03:44 -0500

    Add code lines on model and file pet.js

--- backend/models/pet.js
+++ backend/models/pet.js
@@ -4,6 +4,11 @@ const schema = new mongo.Schema({
     name:string,
     owner:{type:mongo.Schema.ObjectId, ref:"owners"},
     race:{type:mongo.Schema.ObjectId,ref:"type_pet"},
+    registerDate:{
+        type:Date,
+        default:Date.now
+    },
+    dbStatus:Boolean,
 });
 
 const pet = mongo.model("pets", schema);
